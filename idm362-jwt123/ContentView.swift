7aef791552affbf91fededae133e9571f3e5505a
Jervis Thompson <jervo@mac.com>
2025-01-28 07:14:08 -0500

    Added hwk01 SwiftUI

--- idm362-jwt123/ContentView.swift
+++ idm362-jwt123/ContentView.swift
@@ -9,13 +9,26 @@ import SwiftUI
 
 struct ContentView: View {
     var body: some View {
-        VStack {
-            Image(systemName: "globe")
-                .imageScale(.large)
-                .foregroundStyle(.tint)
-            Text("Hello, Jervo!")
+      VStack {
+        MapView()
+          .frame(height: 300)
+        
+        CircleImage()
+          .offset(y: -100)
+          .padding(.bottom, -130)
+        
+        VStack(alignment: .leading) {
+          Text("JERVO - DaLingoKid!")
+            .font(.title)
+            .fontWeight(/*@START_MENU_TOKEN@*/.bold/*@END_MENU_TOKEN@*/)
+            .foregroundColor(Color.mint)
+            .padding()
         }
         .padding()
+        
+        // Push VStack to top of device
+        Spacer()
+      }
     } // End body
 } // End ContentView
 
---
+++ idm362-jwt123/MapView.swift
@@ -0,0 +1,26 @@
+/*
+See the LICENSE.txt file for this sample’s licensing information.
+
+Abstract:
+A view that presents a map.
+*/
+
+import SwiftUI
+import MapKit
+
+struct MapView: View {
+    var body: some View {
+        Map(initialPosition: .region(region))
+    }
+
+    private var region: MKCoordinateRegion {
+        MKCoordinateRegion(
+            center: CLLocationCoordinate2D(latitude: 40.816357, longitude: -73.962898),
+            span: MKCoordinateSpan(latitudeDelta: 0.2, longitudeDelta: 0.2)
+        )
+    }
+}
+
+#Preview {
+    MapView()
+}
